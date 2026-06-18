149dfba033c67c6e9d6aa7e592718c3f8bdb2f80
InnerMonkey <chornyy.im@gmail.com>
2026-06-19 02:50:21 +0300

    feat: Add wire variant list view

---
+++ wire/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import WireWariantListView
+
+app_name = 'wire'
+urlpatterns = [
+    path('variants/', WireWariantListView.as_view(), name='variant-list')
+    
+]
--- wire/views.py
+++ wire/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import WireVariant, WirePiece
+
+class WireWariantListView(ListView):
+    model = WireVariant
+    template_name = 'wires/wirevariant_list.html'
+    context_object_name = 'variants'
+    paginate_by = 50
+
+
 
 # Create your views here.
